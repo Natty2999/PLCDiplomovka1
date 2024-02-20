548bd03b888b3b013525d8c13d80e2d8bf5386f4
Natty2999 <nataliarakovicka@gmail.com>
2024-02-20 23:47:56 +0100

    pridanie tablayout a funkcionalitu prepinania medzi fragment_settings_rameno.xml a fragment_settings_vytah.xml, pouzity addOnTabSelectedListener

--- app/src/main/java/com/example/myapplication/plcdiplomovka1/MainActivity.java
+++ app/src/main/java/com/example/myapplication/plcdiplomovka1/MainActivity.java
@@ -1,24 +1,13 @@
 package com.example.myapplication.plcdiplomovka1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.os.CountDownTimer;
 import android.widget.FrameLayout;
-import android.widget.TextView;
+import com.google.android.material.tabs.TabLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.materialswitch.MaterialSwitch;
-import com.google.android.material.navigation.NavigationBarView;
-
-import Moka7.*;
 //TODO pridat Toasty na upozornenie prebehnutia deju
 //TODO pridat kontrolu ci je IP adresa spravne zadaná (spravny format)
 //TODO pridat kontrolu ci je DBNumber, DBOffset a DBBit spravne zadané (čísla), bit moze byt 0-7
@@ -29,8 +18,13 @@ import Moka7.*;
 public class MainActivity extends AppCompatActivity {
     FrameLayout frameLayout;
     BottomNavigationView bottomNavigationView;
+    TabLayout tabLayoutSettings;
 
     DefaultBitEditing defaultBitEditing = new DefaultBitEditing();
+    VytahFragment vytahFragment = new VytahFragment();
+
+    SettingsVytahFragment settingsVytahFragment = new SettingsVytahFragment();
+    SettingsRamenoFragment settingsRamenoFragment = new SettingsRamenoFragment();
     Testing testing = new Testing();
 
 
@@ -39,20 +33,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        tabLayoutSettings = findViewById(R.id.tabLayoutSettings);
         //how to set default fragment
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, defaultBitEditing).commit();
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
         @Override
         public boolean onNavigationItemSelected( MenuItem item){
            int itemId = item.getItemId();
             if(itemId == R.id.nav_home){
+                tabLayoutSettings.setVisibility(TabLayout.GONE);
                 getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, defaultBitEditing).commit();
             }else if(itemId == R.id.nav_settings){
-                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, testing).commit();
+
+                if(tabLayoutSettings.getSelectedTabPosition() == 0){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsVytahFragment).commit();
+                }else if(tabLayoutSettings.getSelectedTabPosition() == 1){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsRamenoFragment).commit();
+                }
+                tabLayoutSettings.setVisibility(TabLayout.VISIBLE);
+
+            }else if(itemId == R.id.nav_vytah){
+                tabLayoutSettings.setVisibility(TabLayout.GONE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, vytahFragment).commit();
             }
             return true;
         }
+        });
+        tabLayoutSettings.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // This method is called when a tab is selected
+                switch (tab.getPosition()) {
+                    case 0:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsVytahFragment).commit();
+                        break;
+                    case 1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsRamenoFragment).commit();
+                        break;
+                    // Add more cases if you have more tabs
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // This method is called when a tab is unselected
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // This method is called when a tab is reselected
+            }
         });
 
     }
