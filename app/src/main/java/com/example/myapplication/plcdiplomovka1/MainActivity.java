86c132f69ed028a330f6b4801c70120491f3ae7f
Natty2999 <nataliarakovicka@gmail.com>
2024-02-23 17:20:09 +0100

    osetrenie zobrazovania fragmeentov pri meneni orientacie landscape/portrait osetrenie pre prve otvorenie, savedInstanceState
    vytvorenie funkcie pre menenci fragmentu lahsia citatelnost
    
    zmena viditelnosti tablayout pri prepinani fragmentov

--- app/src/main/java/com/example/myapplication/plcdiplomovka1/MainActivity.java
+++ app/src/main/java/com/example/myapplication/plcdiplomovka1/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.myapplication.plcdiplomovka1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+
+
 import com.google.android.material.tabs.TabLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 //TODO pridat Toasty na upozornenie prebehnutia deju
 //TODO pridat kontrolu ci je IP adresa spravne zadaná (spravny format)
 //TODO pridat kontrolu ci je DBNumber, DBOffset a DBBit spravne zadané (čísla), bit moze byt 0-7
@@ -16,7 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 //TODO pridat uchovanie fungujucich IP adries s moznostou pomenovania a vyberu z listu
 
 public class MainActivity extends AppCompatActivity {
-    FrameLayout frameLayout;
+
     BottomNavigationView bottomNavigationView;
     TabLayout tabLayoutSettings;
 
@@ -25,56 +30,57 @@ public class MainActivity extends AppCompatActivity {
 
     SettingsVytahFragment settingsVytahFragment = new SettingsVytahFragment();
     SettingsRamenoFragment settingsRamenoFragment = new SettingsRamenoFragment();
-    Testing testing = new Testing();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         tabLayoutSettings = findViewById(R.id.tabLayoutSettings);
         //how to set default fragment
-        bottomNavigationView.setSelectedItemId(R.id.nav_home);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, defaultBitEditing).commit();
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
-        @Override
-        public boolean onNavigationItemSelected( MenuItem item){
-           int itemId = item.getItemId();
-            if(itemId == R.id.nav_home){
-                tabLayoutSettings.setVisibility(TabLayout.GONE);
-                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, defaultBitEditing).commit();
-            }else if(itemId == R.id.nav_settings){
-
-                if(tabLayoutSettings.getSelectedTabPosition() == 0){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsVytahFragment).commit();
-                }else if(tabLayoutSettings.getSelectedTabPosition() == 1){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsRamenoFragment).commit();
-                }
-                tabLayoutSettings.setVisibility(TabLayout.VISIBLE);
-
-            }else if(itemId == R.id.nav_vytah){
-                tabLayoutSettings.setVisibility(TabLayout.GONE);
-                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, vytahFragment).commit();
-            }
-            return true;
+        // Set the default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, defaultBitEditing).addToBackStack(null).commit();
+            tabLayoutSettings.setVisibility(TabLayout.GONE);
         }
+
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            changeFragment(item);
+            return true;
         });
+
+        // PRIDANE kvôli zobrazeniu tabLayoutu pri zobrazení fragmentu, inak by sa zobrazoval tablaout skor ako sa zobrazil fragment
+        // dava to potom zly vzhlad, taky sekany
+        getSupportFragmentManager().addOnBackStackChangedListener(this::changeTablayoutVisibility);
         tabLayoutSettings.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 // This method is called when a tab is selected
-                switch (tab.getPosition()) {
-                    case 0:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsVytahFragment).commit();
-                        break;
-                    case 1:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, settingsRamenoFragment).commit();
-                        break;
+                Fragment selectedFragment = null;
+
+                if (tab.getPosition() == 0) {
+                    if (settingsVytahFragment == null) {
+                        settingsVytahFragment = new SettingsVytahFragment();
+                    }
+                    selectedFragment = settingsVytahFragment;
+                } else if (tab.getPosition() == 1) {
+                    if (settingsRamenoFragment == null) {
+                        settingsRamenoFragment = new SettingsRamenoFragment();
+                    }
+                    selectedFragment = settingsRamenoFragment;
+                }
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, selectedFragment).addToBackStack(null).commit();
+                }
                     // Add more cases if you have more tabs
                 }
-            }
+
+            //onlayout change to lanscape and back to portrait
+
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
@@ -87,5 +93,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+    public void changeTablayoutVisibility() {
+        if (settingsVytahFragment.isAdded() && settingsVytahFragment.isVisible()) {
+            tabLayoutSettings.setVisibility(TabLayout.VISIBLE);
+        } else if (settingsRamenoFragment.isAdded() && settingsRamenoFragment.isVisible()) {
+            tabLayoutSettings.setVisibility(TabLayout.VISIBLE);
+        } else {
+            tabLayoutSettings.setVisibility(TabLayout.GONE);
+        }
+    }
+    public void changeFragment(MenuItem item){
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_home) {
+            if (defaultBitEditing == null) {
+                defaultBitEditing = new DefaultBitEditing();
+            }
+            selectedFragment = defaultBitEditing;
+        } else if (itemId == R.id.nav_settings) {
+            if (tabLayoutSettings.getSelectedTabPosition() == 0) {
+                if (settingsVytahFragment == null) {
+                    settingsVytahFragment = new SettingsVytahFragment();
+                }
+                selectedFragment = settingsVytahFragment;
+            } else if (tabLayoutSettings.getSelectedTabPosition() == 1) {
+                if (settingsRamenoFragment == null) {
+                    settingsRamenoFragment = new SettingsRamenoFragment();
+                }
+                selectedFragment = settingsRamenoFragment;
+            }
+        } else if (itemId == R.id.nav_vytah) {
+            if (vytahFragment == null) {
+                vytahFragment = new VytahFragment();
+            }
+            selectedFragment = vytahFragment;
+        }
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, selectedFragment).addToBackStack(null).commit();
+        }
+    }
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        changeTablayoutVisibility();
     }
 }
